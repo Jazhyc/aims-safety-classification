bbdc1024655ebaa2756a3218adad06df4a9dc08d
JazHyc <feronest@yahoo.com>
2026-01-19 21:18:25 +0100

    Reduced allocated time for hyper-param sweep

--- scripts/hpc/hyperparam_sweep_template.sh
+++ scripts/hpc/hyperparam_sweep_template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=hyperparam_sweep
-#SBATCH --time=04:00:00
-#SBATCH --mem=32GB
+#SBATCH --time=00:30:00
+#SBATCH --mem=16GB
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --output=logs/slurm/%x-%j.out
