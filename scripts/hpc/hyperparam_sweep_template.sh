cfb26738863a9d8c115f62b73836fa80ca2f62a3
Niclas-J-M <muellerhof.n@googlemail.com>
2026-03-24 16:30:08 +0100

    changed val early stopping in SFT model

--- scripts/hpc/hyperparam_sweep_template.sh
+++ scripts/hpc/hyperparam_sweep_template.sh
@@ -23,4 +23,4 @@ python scripts/baselines/train_generator.py \
     paths.logs_dir="logs/hyperparam_sweep/lr_${LEARNING_RATE}_e_${EPOCHS}" \
     paths.model_save_dir="models/sft/hyperparam_sweep/lr_${LEARNING_RATE}_e_${EPOCHS}" \
     paths.predictions_dir="data/predictions/hyperparam_sweep/lr_${LEARNING_RATE}_e_${EPOCHS}" \
-    wandb.project="intention-jailbreak-hyperparam-sweep"
+    wandb.project="sft-hyperparam-sweep-canonical"
